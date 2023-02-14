762944236c8cf02aa95d671ec72b0bcb93392922
Victor Esteban Mora Arce <themonstermora@gmail.com>
2023-02-14 08:38:09 -0500

    turns and colored by turns added

--- src/Components/GameBoard.js
+++ src/Components/GameBoard.js
@@ -1,8 +1,39 @@
-import React from "react";
+import React, {useState} from "react";
 import GameCircle from "./GameCircle";
 import "../Game.css";
 
+const NO_PLAYER = 0
+const PLAYER_1 = 1
+const PLAYER_2 = 2
+const CIRCLES = 16
+
 const GameBoard = () => {
+
+	const [gameBoard, setGameBoard] = useState(Array(16).fill(NO_PLAYER))
+	const [currentPlayer, setCurrentPlayer] = useState(PLAYER_1)
+
+	const initBoard = () => {
+		let circles = []
+		for (let i=0; i<CIRCLES;i++){
+			circles.push(renderCircle(i))
+		}
+		return circles
+	}
+
+	const circleClicked = (id) => {
+		let board = [...gameBoard]
+		board[id] = currentPlayer
+		setGameBoard(board)
+
+		setGameBoard(prev => {
+			return prev.map((circle, pos) => {
+				if(pos === id) return currentPlayer;
+				return circle;
+			})
+		})
+
+		setCurrentPlayer(currentPlayer === PLAYER_1 ? PLAYER_2 : PLAYER_1)
+	}
 
 	const centereedFlexbox = {
 		display: 'flex',
@@ -10,53 +41,14 @@ const GameBoard = () => {
 		alignItems: 'center'
 	}
 
-	const boardGrid = {
-		display: 'grid',
-		gridTemplateColumns: 'repeat(4,1.5fr)',
-		rowGap: '5px', 
-		height: '500px', 
-		width: '700px'
+	const renderCircle = (id) => {
+		return <GameCircle key={id} id={id} className={`player-${gameBoard[id]}`} onCircleClicked={circleClicked}/>
 	}
 
 	return (
 		<div style={centereedFlexbox}>
 			<div className="gameBoard">
-				<GameCircle id={1}>
-					<div>Red</div>
-				</GameCircle>
-				<GameCircle id={2}>
-					<div>Blue</div>
-				</GameCircle>
-				<GameCircle id={3}>
-					<div>Red</div>
-				</GameCircle>
-				<GameCircle id={4}>
-					<div>Blue</div>
-				</GameCircle>
-				<GameCircle id={5}>
-					<div>Red</div>
-				</GameCircle>
-				<GameCircle id={6}>
-					<div>Blue</div>
-				</GameCircle>
-				<GameCircle id={7}>
-					<div>Red</div>
-				</GameCircle>
-				<GameCircle id={8}>
-					<div>Blue</div>
-				</GameCircle>
-				<GameCircle id={9}>
-					<div>Red</div>
-				</GameCircle>
-				<GameCircle id={10}>
-					<div>Blue</div>
-				</GameCircle>
-				<GameCircle id={11}>
-					<div>Red</div>
-				</GameCircle>
-				<GameCircle id={10}>
-					<div>Blue</div>
-				</GameCircle>
+				{initBoard()}
 			</div>
 		</div>
 	)
--- src/Components/GameCircle.js
+++ src/Components/GameCircle.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import "../Game.css";
 
-const circleClick = (In_Value) => {
-	alert("Clicked "+In_Value)
-}
+const GameCircle = ({id, children, className, onCircleClicked}) => {
 
-const GameCircle = ({id, children}) => {
+
 
 	return (
-		<div className={`gameCircle ${id%2===0 ? "odd-circle" : "even-circle"}`} onClick= {() => circleClick(id)}>
+		<div 
+			className = {`gameCircle ${className}`} 
+			onClick = {() => onCircleClicked(id)}>
 			{children}
 		</div>
 	)
